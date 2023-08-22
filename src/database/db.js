36414ac4d9a3460d8cb742bdd3873ab276357052
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-21 21:32:11 -0300

    modifique el data.js para que me traiga las habitaciones desde mongodb

--- src/database/db.js
+++ src/database/db.js
@@ -1,18 +1,24 @@
 const mongoose = require('mongoose');
+const HabitacionModel = require("../models/Habitacion");
+
 const uri = process.env.PORTDB;
 const db = process.env.DB;
 
 
 const connectDb = async () => {
   try {
-    mongoose.connect(`${uri}/${db}`,{useNewUrlParser: true, useUnifiedTopology: true })
-      .then(() => console.log('estamos conectados a la base de datos'));
+    await mongoose.connect(`${uri}/${db}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log("DB esta conectado");
+
+    const allHabitaciones = await HabitacionModel.find(); //buscamos todos los documentos de la coleccion
+    console.log(allHabitaciones);
+
 
   }
-  
- catch (error) {
-  console.log(error);
-}
+
+  catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connectDb;
