5687572d08cb743b3a11bca02fed4eee2ad018b4
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-22 01:55:13 -0300

    defini las rutas de las funcionalidades del controller de usuarios en UsuariosRoutes

--- src/routes/UsuariosRoutes.js
+++ src/routes/UsuariosRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuariosController');
 
+// Rutas para usuarios
 router.get('/usuarios', usuariosController.getAllUsuarios);
-// Agregar más rutas para crear, actualizar y eliminar usuarios
+router.post('/usuarios', usuariosController.crearUsuario);
+router.get('/usuarios/:id', usuariosController.obtenerUsuarioPorId);
+router.put('/usuarios/:id', usuariosController.actualizarUsuario);
+router.delete('/usuarios/:id', usuariosController.eliminarUsuario);
 
 module.exports = router;
