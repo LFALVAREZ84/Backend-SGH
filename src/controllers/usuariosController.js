1278ce64cfbada8c8ad9de3889a78ee74c61a81a
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-22 01:51:16 -0300

    agregue usuariosControlller  con nuevas funciones

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,4 +1,4 @@
-const Usuario = require('../models/Usuario.JS');
+const Usuario = require('../models/Usuario'); // Importa el modelo de usuarios
 
 const usuariosController = {
   getAllUsuarios: async (req, res) => {
@@ -10,8 +10,59 @@ const usuariosController = {
     }
   },
 
-  // Otros métodos del controlador para crear, actualizar y eliminar usuarios
+  // Crear un nuevo usuario
+  crearUsuario: async (req, res) => {
+    try {
+      const nuevoUsuario = await Usuario.create(req.body);
+      res.status(201).json(nuevoUsuario);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al crear el usuario' });
+    }
+  },
+
+  // Obtener un usuario por su ID
+  obtenerUsuarioPorId: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const usuario = await Usuario.findById(id);
+      if (!usuario) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+      res.json(usuario);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al obtener el usuario' });
+    }
+  },
+
+  // Actualizar un usuario por su ID
+  actualizarUsuario: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const usuarioActualizado = await Usuario.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      if (!usuarioActualizado) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+      res.json(usuarioActualizado);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al actualizar el usuario' });
+    }
+  },
+
+  // Eliminar un usuario por su ID
+  eliminarUsuario: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const usuarioEliminado = await Usuario.findByIdAndDelete(id);
+      if (!usuarioEliminado) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+      res.json({ message: 'Usuario eliminado exitosamente' });
+    } catch (error) {
+      res.status(500).json({ error: 'Error al eliminar el usuario' });
+    }
+  },
 };
 
 module.exports = usuariosController;
-
