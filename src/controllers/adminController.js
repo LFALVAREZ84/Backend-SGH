707aac940b3c61f5f3b367cfb461ba117f5c8144
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-23 20:35:29 -0300

    agregue funcionalidades al admin controller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,15 +1,55 @@
 const Habitacion = require('../models/Habitacion');
 
-const clienteController = {
-  obtenerHabitacionesDisponibles: async (req, res) => {
+const adminController = {
+  // Obtener todas las habitaciones
+  obtenerTodasLasHabitaciones: async (req, res) => {
     try {
-      const habitaciones = await Habitacion.find({ disponible: true });
+      const habitaciones = await Habitacion.find();
       res.status(200).json(habitaciones);
     } catch (error) {
-      res.status(500).json({ error: 'Error al obtener las habitaciones disponibles' });
+      res.status(500).json({ error: 'Error al obtener las habitaciones' });
     }
   },
-  // Otras funciones para clientes
+
+  // Crear una nueva habitación
+  crearHabitacion: async (req, res) => {
+    try {
+      const nuevaHabitacion = await Habitacion.create(req.body);
+      res.status(201).json(nuevaHabitacion);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al crear la habitación' });
+    }
+  },
+
+  // Actualizar una habitación por su ID
+  actualizarHabitacion: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const habitacionActualizada = await Habitacion.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      if (!habitacionActualizada) {
+        return res.status(404).json({ error: 'Habitación no encontrada' });
+      }
+      res.status(200).json(habitacionActualizada);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al actualizar la habitación' });
+    }
+  },
+
+  // Eliminar una habitación por su ID
+  eliminarHabitacion: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const habitacionEliminada = await Habitacion.findByIdAndDelete(id);
+      if (!habitacionEliminada) {
+        return res.status(404).json({ error: 'Habitación no encontrada' });
+      }
+      res.status(200).json({ message: 'Habitación eliminada exitosamente' });
+    } catch (error) {
+      res.status(500).json({ error: 'Error al eliminar la habitación' });
+    }
+  },
 };
 
-module.exports = clienteController;
+module.exports = adminController;
