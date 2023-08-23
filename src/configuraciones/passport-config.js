27fdbd3d0c3e144aca77cd23b5a2201e2a450677
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-23 20:56:59 -0300

    agregue config de passport

--- src/configuraciones/passport-config.js
+++ src/configuraciones/passport-config.js
@@ -3,12 +3,37 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const Usuario = require('./models/Usuario'); // Asegúrate de tener la ruta correcta al modelo Usuario
 
+// Estrategia de autenticación para usuarios normales
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
       const usuario = await Usuario.findOne({ email: username });
       if (!usuario) {
         return done(null, false, { message: 'Usuario no encontrado' });
+      }
+      const match = await bcrypt.compare(password, usuario.contraseña);
+      if (match) {
+        return done(null, usuario);
+      } else {
+        return done(null, false, { message: 'Contraseña incorrecta' });
+      }
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+// Estrategia de autenticación para administradores
+passport.use(
+  'local-admin',
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const usuario = await Usuario.findOne({ email: username });
+      if (!usuario) {
+        return done(null, false, { message: 'Usuario no encontrado' });
+      }
+      if (usuario.rol !== 'admin') {
+        return done(null, false, { message: 'Acceso no autorizado' });
       }
       const match = await bcrypt.compare(password, usuario.contraseña);
       if (match) {
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport'); // Importa Passport
 const adminController = require('../controllers/adminController');
 
 // Obtener todas las habitaciones
-router.get('/habitaciones', adminController.obtenerTodasLasHabitaciones);
+router.get('/habitaciones',  passport.authenticate('local'), adminController.obtenerTodasLasHabitaciones);
 
 // Crear una nueva habitación
-router.post('/habitaciones', adminController.crearHabitacion);
+router.post('/habitaciones',  passport.authenticate('local'),adminController.crearHabitacion);
 
 // Actualizar una habitación por su ID
-router.put('/habitaciones/:id', adminController.actualizarHabitacion);
+router.put('/habitaciones/:id',  passport.authenticate('local'), adminController.actualizarHabitacion);
 
 // Eliminar una habitación por su ID
 router.delete('/habitaciones/:id', adminController.eliminarHabitacion);
 // Obtener todas las reservas
-router.get('/reservas', adminController.obtenerTodasLasReservas);
+router.get('/reservas', passport.authenticate('local'), adminController.obtenerTodasLasReservas);
 
 // Cancelar una reserva por su ID
-router.put('/reservas/:reservaId', adminController.cancelarReserva);
+router.put('/reservas/:reservaId', passport.authenticate('local'), adminController.cancelarReserva);
 
 // Crear una nueva reserva
-router.post('/reservas', adminController.crearReserva);
+router.post('/reservas',  passport.authenticate('local'),adminController.crearReserva);
 
 
 module.exports = router;
