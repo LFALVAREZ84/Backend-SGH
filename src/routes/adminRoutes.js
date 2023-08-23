26d49fd68a2d2e1c3e093cd15543b12c95c4dfef
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-23 20:49:22 -0300

    agregue ruta de reservas

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -13,5 +13,14 @@ router.put('/habitaciones/:id', adminController.actualizarHabitacion);
 
 // Eliminar una habitación por su ID
 router.delete('/habitaciones/:id', adminController.eliminarHabitacion);
+// Obtener todas las reservas
+router.get('/reservas', adminController.obtenerTodasLasReservas);
+
+// Cancelar una reserva por su ID
+router.put('/reservas/:reservaId', adminController.cancelarReserva);
+
+// Crear una nueva reserva
+router.post('/reservas', adminController.crearReserva);
+
 
 module.exports = router;
